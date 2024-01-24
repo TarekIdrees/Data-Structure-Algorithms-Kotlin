9323a08717f634f3e74675ee2ae443114eea06b4
TarekIdrees <tarekidrees5@gmail.com>
2024-01-24 09:39:21 +0100

    implement insertion sort algorithm

---
+++ src/main/kotlin/algorithms/Insertion sort.kt
@@ -0,0 +1,17 @@
+package algorithms
+
+fun <T : Comparable<T>> insertionSort(array: Array<T>, ascending: Boolean = true): Array<T> {
+    var key: T
+    var j: Int
+    for (i in 1 until array.size) {
+        key = array[i]
+        j = i - 1
+        while (j >= 0 && ((ascending && array[j] > key) || (!ascending && array[j] < key))) {
+            array[j + 1] = array[j]
+            j -= 1
+        }
+
+        array[j + 1] = key
+    }
+    return array
+}
--- src/main/kotlin/algorithms/Main.kt
+++ src/main/kotlin/algorithms/Main.kt
@@ -3,7 +3,8 @@ package algorithms
 fun main() {
     var array = arrayOf(80, 20, 70, 90, 2, 55)
     //array = selectionSort(array,true)
-   array = bubbleSort(array,false)
+   //array = bubbleSort(array,false)
+    array = insertionSort(array,false)
     array.forEach {
         print("$it,")
     }
