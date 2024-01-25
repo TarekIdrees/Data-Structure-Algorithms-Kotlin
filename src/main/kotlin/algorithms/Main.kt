5ff4a3d21d854c4ee1ce80e275ee544670f6de6b
TarekIdrees <tarekidrees5@gmail.com>
2024-01-25 13:48:53 +0100

    implement merge sort algorithm

--- src/main/kotlin/algorithms/Main.kt
+++ src/main/kotlin/algorithms/Main.kt
@@ -3,9 +3,10 @@ package algorithms
 fun main() {
     var array = arrayOf(80, 20, 70, 90, 2, 55)
     //array = selectionSort(array,true)
-   //array = bubbleSort(array,false)
-    array = insertionSort(array,false)
-    array.forEach {
+    //array = bubbleSort(array,false)
+    // array = insertionSort(array,false)
+    val sortedArray = mergeSort(array.toIntArray())
+    sortedArray.forEach {
         print("$it,")
     }
 }
---
+++ src/main/kotlin/algorithms/Merge Sort.kt
@@ -0,0 +1,50 @@
+package algorithms
+
+fun mergeSort(
+    array: IntArray,
+    firstIndex: Int = 0,
+    lastIndex: Int = array.size - 1,
+): IntArray {
+    if (array.size <= 1) {
+        return array
+    }
+    val middleIndex = (firstIndex + lastIndex) / 2
+    val leftArray = array.copyOfRange(firstIndex, middleIndex + 1)
+    val rightArray = array.copyOfRange(middleIndex + 1, lastIndex + 1)
+
+    return merge(mergeSort(leftArray), mergeSort(rightArray))
+}
+
+private fun merge(
+    leftArray: IntArray,
+    rightArray: IntArray,
+): IntArray {
+    val mergedArray = IntArray(leftArray.size + rightArray.size)
+    var leftIndex = 0
+    var rightIndex = 0
+    var mergedIndex = 0
+
+    while (leftIndex < leftArray.size && rightIndex < rightArray.size) {
+        if (leftArray[leftIndex] <= rightArray[rightIndex]) {
+            mergedArray[mergedIndex] = leftArray[leftIndex]
+            leftIndex++
+        } else {
+            mergedArray[mergedIndex] = rightArray[rightIndex]
+            rightIndex++
+        }
+        mergedIndex++
+    }
+
+    while (leftIndex < leftArray.size) {
+        mergedArray[mergedIndex] = leftArray[leftIndex]
+        leftIndex++
+        mergedIndex++
+    }
+
+    while (rightIndex < rightArray.size) {
+        mergedArray[mergedIndex] = rightArray[rightIndex]
+        rightIndex++
+        mergedIndex++
+    }
+    return mergedArray
+}
