dbd500e7a821c55fa950defbb2b9527352e5e010
TarekIdrees <tarekidrees5@gmail.com>
2024-01-25 17:28:51 +0100

    implement heap sort algorithm

---
+++ src/main/kotlin/algorithms/Heap Sort.kt
@@ -0,0 +1,33 @@
+package algorithms
+
+fun <T : Comparable<T>> heapSort(array: Array<T>, ascending: Boolean = true): Array<T> {
+    buildHeap(array, array.size, ascending)
+    for (i in array.size - 1 downTo 0) {
+        swap(array, 0, i)
+        heapify(array, i, 0, ascending)
+    }
+    return array
+}
+
+private fun <T : Comparable<T>> buildHeap(array: Array<T>, size: Int, ascending: Boolean) {
+    for (i in (size / 2) - 1 downTo 0) {
+        heapify(array, size, i, ascending)
+    }
+}
+
+private fun <T : Comparable<T>> heapify(array: Array<T>, size: Int, i: Int, ascending: Boolean) {
+    val l = 2 * i + 1
+    val r = 2 * i + 2
+    var max = i
+    if (l < size && (ascending && array[l] > array[max] || !ascending && array[l] < array[max])) {
+        max = l
+    }
+
+    if (r < size && (ascending && array[r] > array[max] || !ascending && array[r] < array[max])) {
+        max = r
+    }
+    if (max != i) {
+        swap(array, i, max)
+        heapify(array, size, max, ascending)
+    }
+}
--- src/main/kotlin/algorithms/Main.kt
+++ src/main/kotlin/algorithms/Main.kt
@@ -4,12 +4,10 @@ fun main() {
     var array = arrayOf(80, 20, 70, 90, 2, 55)
     //array = selectionSort(array,true)
     //array = bubbleSort(array,false)
-    // array = insertionSort(array,false)
-   // val sortedArray = mergeSort(array.toIntArray())
-//    sortedArray.forEach {
-//        print("$it,")
-//    }
-    array = quickSort(array,0,array.lastIndex, ascending = true)
+    //array = insertionSort(array,false)
+    //array = mergeSort(array.toIntArray()).toTypedArray()
+    //array = quickSort(array,0,array.lastIndex, ascending = true)
+    //array = heapSort(array, ascending = false)
     array.forEach {
         print("$it,")
     }
